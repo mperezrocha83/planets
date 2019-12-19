0932d4455ea9001f254dfb9629d200e2474c639e
mperezrocha83 <mperezrocha@ou.edu>
2019-12-19 11:51:40 -0600

    Added plot for venus

--- mars.R
+++ mars.R
@@ -4,5 +4,6 @@
 ##analyze color of MArs
 
 red <- read.csv("color_spreadsheet.csv")### import file
-plot (red) ## make a red plot
-venus <- planets [,2] #adding venus 
+plot(red) ## make a red plot
+venus <- planets[,2] #adding venus 
+plot(venus)
